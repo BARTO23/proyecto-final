699acc85b6eb170e29f3d4854c1e842287bd73e3
Juan Pablo Tobar <juanpablococtm@gmail.com>
2024-05-22 16:31:01 -0500

    22/05/2024 4:29 pm, Creación del repositorio, creación de la carpeta code, creacion del modulo paciente, creación de los metodos getters y setters

---
+++ code/cls_pacientes.java
@@ -0,0 +1,17 @@
+public class cls_pacientes{
+  private String nombreMascotaStr;
+  private String idStr;
+  private int edadInt;
+  private String contactoStr;
+  private String historiaStr;
+
+  public cls_pacientes(String nombreMascotaStr, String idStr, int edadInt, String contactoStr, String historiaStr){
+    this.nombreMascotaStr = nombreMascotaStr;
+    this.idStr = idStr;
+    this.edadInt = edadInt;
+    this.contactoStr = contactoStr;
+    this.historiaStr = historiaStr;
+  }
+
+  
+}
